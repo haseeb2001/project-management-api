61c4bfdcc811a17a5c57717e3d21dad258e5c0d0
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2023-07-11 18:39:50 +0500

    Added routes for tasks

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const UserRouter = require('./user.routes');
+const TaskRouter = require('./task.routes')
 
 const router = express.Router();
 
 router.use('/auth', UserRouter);
+router.use('/tasks', TaskRouter)
 
 module.exports = router;
